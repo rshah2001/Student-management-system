7c097ba1d0bac8b14cac90f00456f5f7e83b108d
pavankumarm2505 <pavankumarm2505@gmail.com>
2024-08-09 22:18:43 -0400

    added the about section

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ class MainWindow(QMainWindow):
         about_action = QAction("About", self)
         help_menu_item.addAction(about_action)
         about_action.setMenuRole(QAction.MenuRole.NoRole)
+        about_action.triggered.connect(self.about)
 
         search_action = QAction(QIcon("icons/search.png"), "Search", self)
         search_menu_item.addAction(search_action)
@@ -89,6 +90,10 @@ class MainWindow(QMainWindow):
 
     def delete(self):
         dialog = DeleteDialog()
+        dialog.exec()
+
+    def about(self):
+        dialog = AboutDialog()
         dialog.exec()
 
 
@@ -178,6 +183,16 @@ class SearchDialog(QDialog):
         connection.close()
 
 
+class AboutDialog(QMessageBox):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("About")
+        content = """
+        This app was created to help manage database
+        """
+        self.setText(content)
+
+
 class EditDialog(QDialog):
     def __init__(self):
         super().__init__()
@@ -234,6 +249,7 @@ class EditDialog(QDialog):
         # refresh the table
         main_window.load_data()
 
+
 class DeleteDialog(QDialog):
     def __init__(self):
         super().__init__()
@@ -279,7 +295,6 @@ class DeleteDialog(QDialog):
         confirmation_widget.setWindowTitle("Record Deleted")
         confirmation_widget.setText("The selected record has been deleted.")
         confirmation_widget.exec()
-
 
 
 app = QApplication(sys.argv)
